d3b3e3d9403215ee8d27a9defdb4c4efab042cc2
0taepark <ytpark1665@gmail.com>
2023-01-02 20:44:20 +0900

    버튼생성

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,6 +8,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -43,9 +44,52 @@ const Bio = () => {
         alt="Profile picture"
       />
       
-      <h3>안녕하세요. <br/><strong>프론트엔드 개발자 박영태</strong>입니다. </h3>
+      <h3>안녕하세요. <br/><strong>프론트엔드 개발자 박영태</strong>입니다.</h3>
+      <Button><a href="https://github.com/0taepark">Github</a></Button>
+      <Button><a href="mailto:ytpark1665@gmail.com">Gmail</a></Button>
     </div>
   )
 }
 
 export default Bio
+
+const Button = styled.button`
+
+  background-color: #fff;
+  border: 0 solid #e2e8f0;
+  border-radius: 1.5rem;
+  box-sizing: border-box;
+  color: #0d172a;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Basier circle",-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 1.1rem;
+  font-weight: 600;
+  line-height: 1;
+  padding: 1rem 1.6rem;
+  text-align: center;
+  text-decoration: none #0d172a solid;
+  text-decoration-thickness: auto;
+  transition: all .1s cubic-bezier(.4, 0, .2, 1);
+  box-shadow: 0px 1px 2px rgba(166, 175, 195, 0.25);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+
+
+:hover {
+  background-color: #1e293b;
+  color: #fff;
+}
+
+@media (min-width: 768px) {
+  .button-81 {
+    font-size: 1.125rem;
+    padding: 1rem 2rem;
+  }
+
+  a {
+    text-decoration: none;
+    all:unset;
+  }
+}`
